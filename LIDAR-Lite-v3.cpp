039740876816741be730fa36989f33dfcd0366d2
chisl <info@chisl.io>
2017-12-27 18:14:23 +0100

    Initial commit

---
+++ LIDAR-Lite-v3.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        LIDAR-Lite-v3
+ * description: A compact, high-performance optical distance measurement sensor from Garmin™.
+ * manuf:       Garmin
+ * version:     0.1
+ * url:         https://static.garmin.com/pumac/LIDAR_Lite_v3_Operation_Manual_and_Technical_Specifications.pdf
+ * date:        2017-12-15
+ * author       https://chisl.io/
+ * file:        LIDAR-Lite-v3.cpp
+ */
+
+#include "LIDAR-Lite-v3.hpp"
+
